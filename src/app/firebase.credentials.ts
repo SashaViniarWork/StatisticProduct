c5999cfae3a2be9967bb5f5e64308eaac9c61bf3
sasha.viniar <vinar262@gmail.com>
2018-05-14 11:23:57 +0300

    first commit

---
+++ src/app/firebase.credentials.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyCBa7xwM_TRfoLPks5dll6dbDtYu8J9HRY",
+  authDomain: "dream-f647b.firebaseapp.com",
+  databaseURL: "https://dream-f647b.firebaseio.com",
+  projectId: "dream-f647b",
+  storageBucket: "dream-f647b.appspot.com",
+  messagingSenderId: "339255845553"
+}
